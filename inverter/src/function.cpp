d3cc39ed417616a34e3119b903b684a858c76c26
Julio <julio.engcomp@gmail.com>
2021-06-11 11:06:37 -0300

    Update function.cpp

--- inverter/src/function.cpp
+++ inverter/src/function.cpp
@@ -10,9 +10,5 @@ using std::iter_swap;
 template <size_t SIZE>
 void reverse( std::array< std::string, SIZE > & arr )
 {
-    auto first = arr.begin();
-    auto last = arr.last();
-
-    while( first != last and first != --last )
-        std::iter_swap( first++, last );
+    /*codigo*/
 }
